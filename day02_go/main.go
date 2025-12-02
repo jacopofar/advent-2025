90a9eb191f2727cf43fda730c6a71fea1d3a01c9
Jacopo Farina <jacopo1.farina@gmail.com>
2025-12-02 11:50:06 +0100

    Complete day 2

--- day02_go/main.go
+++ day02_go/main.go
@@ -11,23 +11,28 @@ import (
 func IsInvalid(x int, onlyTwo bool) bool {
 	var repr = strconv.Itoa(x)
 	var sl = len(repr)
-	if onlyTwo {
-		if sl%2 == 0 {
-			if repr[:sl/2] == repr[sl/2:] {
-				return true
-			}
+
+	for splitLengthCandidate := sl / 2; splitLengthCandidate >= 1; splitLengthCandidate-- {
+		// non-exact split
+		if sl%splitLengthCandidate != 0 {
+			continue
 		}
-	} else {
-		// TODO
-		return true
+		if repr == strings.Repeat(repr[:splitLengthCandidate], sl/splitLengthCandidate) {
+			return true
+		}
+		if onlyTwo {
+			// stop iterating, they asked for only a split into two and this is not it
+			return false
+		}
 	}
+
 	return false
 }
 
-func RepetitionsInRangeBruteForce(f int, t int) int {
+func RepetitionsInRangeBruteForce(f int, t int, onlyTwo bool) int {
 	var total int = 0
 	for x := f; x <= t; x++ {
-		if IsInvalid(x, true) {
+		if IsInvalid(x, onlyTwo) {
 			total += x
 		}
 	}
@@ -41,7 +46,7 @@ func main() {
 	}
 	input := strings.Trim(string(b_input), " \n")
 	var intervals = strings.Split(input, ",")
-	var solution1 int = 0
+	var solution1, solution2 int = 0, 0
 	for _, interval := range intervals {
 		var nums = strings.Split(interval, "-")
 		f, err := strconv.Atoi(nums[0])
@@ -52,8 +57,10 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		solution1 += RepetitionsInRangeBruteForce(f, t)
+		solution1 += RepetitionsInRangeBruteForce(f, t, true)
+		solution2 += RepetitionsInRangeBruteForce(f, t, false)
+
 	}
 	fmt.Printf("SOLUTION PART 1: %d", solution1)
-
+	fmt.Printf("SOLUTION PART 2: %d", solution2)
 }
